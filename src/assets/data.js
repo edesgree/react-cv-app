4cabe22cef4d4f17224b6fa4598c8377cce3563a
Edouard Desgrée <edouard.desgree@gmail.com>
2023-01-25 08:08:25 +0100

    dummy in data.js

---
+++ src/assets/data.js
@@ -0,0 +1,55 @@
+const data = {
+  firstName: 'edouard',
+  lastName: 'desgree',
+  userJobTitle: 'front end developer',
+  userDescription:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facer consectetur dolores accusantium error blanditiis quaerat deserunt  asperiores, necessitatibus, itaque aut voluptates eveniet venia.',
+  email: 'edouard@desgree.com',
+  phone: '345-098-3333',
+  localisation: 'Montreal,Qc',
+  socials: [
+    {
+      name: 'facebook',
+      link: 'www.facebook.com'
+    },
+    {
+      name: 'github',
+      link: 'www.github.com'
+    },
+    {
+      name: 'instagram',
+      link: 'www.instagram.com'
+    }
+  ],
+  interests: ['badminton', 'rock climbing', 'space science'],
+  work: [
+    {
+      dateStart: '02-02-2020',
+      dateEnd: '01-01-2022',
+      jobTitle: 'webdesigner',
+      company: 'designCo',
+      description:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing eli Excepturi provident tenetur voluptatibus dolorum facere quo consectetur.',
+      location: 'Paris, France'
+    },
+    {
+      dateStart: '16-12-2018',
+      dateEnd: '24-09-2022',
+      jobTitle: 'adventurer',
+      company: 'popoy',
+      description:
+        'Adventure ipsum dolor sit amet consectetur, adipisicing eli Excepturi provident tenetur voluptatibus dolorum facere quo consectetur.',
+      location: 'Auckland, Nz'
+    }
+  ],
+  education: [
+    {
+      dateGraduated: '16-12-2008',
+      diplomaTitle: 'Maitrise audiovisuel & multimedia',
+      schoolName: 'University of Marne la vallée',
+      notes: 'some notes about the curriculum'
+    }
+  ]
+};
+
+export default data;
