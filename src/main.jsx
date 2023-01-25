e3c3ae2a7772f04945e778080c58b68694563d94
Edouard Desgrée <edouard.desgree@gmail.com>
2023-01-25 06:41:05 +0100

    add fontawesome icons

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './assets/styles.scss';
 import 'bulma/css/bulma.min.css';
+//import 'bulma/bulma.sass';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
