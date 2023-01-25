d803b26bda38a0132dd3fdd30b751836ca2e6dd6
Edouard Desgrée <edouard.desgree@gmail.com>
2023-01-25 05:48:34 +0100

    install bulma css

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,17 @@ function App() {
     <main className="App">
       <header>
         <h1>hello</h1>
+        <div class="buttons">
+          <button class="button is-primary">Primary</button>
+          <button class="button is-link">Link</button>
+        </div>
+
+        <div class="buttons">
+          <button class="button is-info">Info</button>
+          <button class="button is-success">Success</button>
+          <button class="button is-warning">Warning</button>
+          <button class="button is-danger">Danger</button>
+        </div>
       </header>
     </main>
   );
